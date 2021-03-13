2c848507d81c658da415ce08ff3beeec4f22c0aa
Patrick Weiss <info@tekgator.com>
2021-03-13 17:15:21 +0000

    Add ATA/SCSI environment options

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-CONFIG_FILENAME="hd-idle.config"
-
-if [ ! -f $CONFIG_FILENAME ]; then
+if [ ! -f ./hd-idle.config ]; then
+    cp $APP_PATH/hd-idle.config .
+    
     HD_IDLE_OPTS=
 
     if [ ! -z "$IDLE_TIME" ]; then
@@ -17,38 +17,44 @@ if [ ! -f $CONFIG_FILENAME ]; then
 
         m="IDLE_TIME${i}"
         declare -n IDLE_TIMEX="$m"
-        IDLE_TIMEX=${IDLE_TIMEX:-60}
-        
-        HD_IDLE_OPTS="$HD_IDLE_OPTS -a $DISK_IDX -i $IDLE_TIMEX"
-    done
+        IDLE_TIMEX=${IDLE_TIMEX:-600}
 
-    LOGGING=${LOGGING:-0}
-    if [ "$LOGGING" != "0" ]; then
-        HD_IDLE_OPTS="$HD_IDLE_OPTS -l $CONFIG_PATH/hd-idle.log"
-    fi
+        HD_IDLE_OPTS="$HD_IDLE_OPTS -a $DISK_IDX -i $IDLE_TIMEX"
+
+        o="DISK_CMD${i}"
+        declare -n DISK_CMDX="$o"
+
+        if [ ! -z "$DISK_CMDX" ]; then
+            HD_IDLE_OPTS="$HD_IDLE_OPTS -c ${DISK_CMDX,,}"
+        fi
+    done
 
     HD_IDLE_OPTS=${HD_IDLE_OPTS## } # remove leading spaces
     HD_IDLE_OPTS=${HD_IDLE_OPTS%% } # remove trailing spaces
 
-    if [ -z "$HD_IDLE_OPTS" ]; then
-        cp -v /etc/default/hd-idle ./$CONFIG_FILENAME
-    else
-        echo START_HD_IDLE=true > ./$CONFIG_FILENAME
-        echo HD_IDLE_OPTS="\"$HD_IDLE_OPTS\"" >> ./$CONFIG_FILENAME
-    fi
-
-    chmod -v ug+rwx ./$CONFIG_FILENAME
-    chown -v root:users ./$CONFIG_FILENAME
+    # write HD_IDLE_OPTS to file
+    echo HD_IDLE_OPTS="\"$HD_IDLE_OPTS\"" >> ./hd-idle.config
+    chmod -v ug+rwx ./hd-idle.config
+    chown -v root:users ./hd-idle.config
 
     echo "Created '$CONFIG_PATH/$CONFIG_FILENAME' file with content:"
-    cat ./$CONFIG_FILENAME
+    
+    echo
+    echo "BEGIN-----------------------------------------------------"
+    echo
+    
+    cat ./hd-idle.config 
+
+    echo
+    echo "END-------------------------------------------------------"
+    echo
+
+    HD_IDLE_OPTS=
 fi
 
-source ./$CONFIG_FILENAME
-echo "Run hd-idle"
-echo $START_HD_IDLE
-echo $HD_IDLE_OPTS
-hd-idle -d
-echo "After hd-idle"
+source ./hd-idle.config
 
-trap : TERM INT; sleep infinity & wait
+echo "Run hd-idle with options:"
+echo "HD_IDLE_OPTS=$HD_IDLE_OPTS"
+echo
+hd-idle -d $HD_IDLE_OPTS
